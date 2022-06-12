dc5584ea8f8a33943559377847447c45cad41412
Jenny <jfperham@gmail.com>
2022-06-12 07:35:10 -0700

    defined and rendered two components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import StudentList from "./components/StudentList";
+import ClassInfo from "./components/ClassInfo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Attendance</h1>
+      <ClassInfo></ClassInfo>
+      Here's the first rendered student list:
+      <StudentList></StudentList>
+      Here's the second rendered student list:
+      <StudentList></StudentList>
+    </main>
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <p>
+    //         Edit <code>src/App.js</code> and save to reload.
+    //       </p>
+    //       <a
+    //         className="App-link"
+    //         href="https://reactjs.org"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Learn React
+    //       </a>
+    //     </header>
+    //   </div>
   );
 }
 
 export default App;
+
+// Alternate way to do the same thing:
+// function App() {
+//   const studentList = <StudentList></StudentList>;
+//   return (
+//     <main>
+//       <h1>Attendance</h1>
+//       {studentList}
+//     </main>
+//   );
+// }
+
+// export default App;
