24291f4b0a484f137add1311f897efd3290e75a0
Jenny <jfperham@gmail.com>
2022-06-12 08:24:20 -0700

    added some css

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,7 +1,34 @@
 import React from "react";
+import "./StudentList.css";
 
 const StudentList = () => {
-  return <h2>Student List</h2>;
+  return (
+    <section>
+      <h2 className="student-list__heading">Student List</h2>
+      <ul className="student-list">
+        <li>Student A</li>
+        <li>Student B</li>
+        <li>Student C</li>
+      </ul>
+    </section>
+  );
 };
 
 export default StudentList;
+
+// Alternate way to do this:
+// const StudentList = () => {
+//   const headingClass = 'student-list__heading';
+//   const listClass = 'student-list';
+
+//   return (
+//       <section>
+//           <h2 className={headingClass}>Student List</h2>
+//           <ul className={listClass}>
+//               <li>Student A</li>
+//               <li>Student B</li>
+//               <li>Student C</li>
+//           </ul>
+//       </section>
+//   )
+// }
